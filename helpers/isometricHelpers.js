9a69b542a48e7659655471447b701c775a32f5ca
unknown <vbleton@FR100WL04135.tp.tpnet.intra>
2016-01-02 12:26:55 +0100

    Create ReportUnicIso script Update isometricHelpers

--- helpers/isometricHelpers.js
+++ helpers/isometricHelpers.js
@@ -16,6 +16,7 @@ function Isometric(name, relatedTags, impactedIsometrics) {
 }
 
 Isometric.prototype.updateOnHoldCount = function (instruments, vendorDocs) {
+  var self = this;
   var onHoldCount = 0;
   if (this.relatedTags.length > 0) {
     this.relatedTags.forEach(function(relatedTag) {
@@ -26,6 +27,7 @@ Isometric.prototype.updateOnHoldCount = function (instruments, vendorDocs) {
         var relatedvendorDoc = vendorDocs.filter(function(vendorDoc) {
           return vendorDoc.ref == relatedInstrument.gad;
         })[0];
+
         if (typeof relatedvendorDoc != 'undefined') {
           if ((typeof relatedvendorDoc.latestRevision.statusCode != 'number') || (typeof relatedInstrument.pdmsStatus  != 'number') || (relatedvendorDoc.latestRevision.statusCode < 2) || (relatedInstrument.pdmsStatus == 1)) {
             // TODO : gérer le cas PDMS non setté
@@ -33,6 +35,9 @@ Isometric.prototype.updateOnHoldCount = function (instruments, vendorDocs) {
             onHoldCount++;
           }
         }
+      } else {
+        onHoldCount++;
+        // TODO : gérer le cas où le tag est dans le PDMS et pas dans le SPI
       }
     });
   }
@@ -44,7 +49,7 @@ Isometric.prototype.updateOnHoldImpactedIsoCount = function (isometrics) {
   if (this.impactedIsometrics.length > 0) {
     this.impactedIsometrics.forEach(function(relatedIso) {
       var impactedIso = isometrics.filter(function(iso) {
-        return (iso.name == relatedIso);
+        return (iso.name == relatedIso.impactedIso);
       })[0];
       if (impactedIso.onHoldCount > 0) {
         onHoldImpactedIsoCount++;
@@ -73,7 +78,7 @@ var importIsometrics = function(bomData, pdmsData, impactedIsoData) {
 
     if (typeof a != 'undefined') {
       var listImpactedIso = a.map(function(l) {
-        return l[4];
+        return {impactedIso : l[4], impactingTag: l[1]};
       });
     } else {
       var listImpactedIso = [];
@@ -98,13 +103,42 @@ var importIsometrics = function(bomData, pdmsData, impactedIsoData) {
   return newLines;
 }
 
+// fonction d'import pour un isométrique unique appelé IsoName
+var uniqueExportFunction = function(isometrics, instruments, vendorDocs, isoName) {
+  var targetedIso = isometrics.filter(function(iso) {
+    return iso.name == isoName;
+  })[0];
+
+  var temp1 = targetedIso.relatedTags.map(function(relatedTagName) {
+
+    var relatedTag = instruments.filter(function(instrum) {
+      return instrum.tag == relatedTagName;
+    })[0];
+
+    var relatedVendorDoc = vendorDocs.filter(function(vendorDoc) {
+      return vendorDoc.ref == relatedTag.gad;
+    })[0];
+    return [relatedTag.tag, relatedVendorDoc.latestRevision.statusCode, relatedTag.pdmsStatus]
+  });
+
+
+  var temp2 = targetedIso.impactedIsometrics.map(function(iso) {
+    var impactedIso = isometrics.filter(function(is) {
+      return is.name == iso.impactedIso;
+    })[0];
+    return [iso.impactedIso, iso.impactingTag , impactedIso.IFCStatus]
+  });
+  return [temp1, temp2];
+}
 
 var exportFunction = function(isometric) {
   return [isometric.name,isometric.onHoldCount,isometric.onHoldImpactedIsoCount, isometric.IFCStatus];
 }
 
+
 module.exports = {
   importIsometrics : importIsometrics,
+  uniqueExportFunction : uniqueExportFunction,
   Isometric : Isometric,
   exportFunction : exportFunction
 };
---
+++ scriptReportUnicIso.js
@@ -0,0 +1,61 @@
+var xlsx = require('node-xlsx');
+var fs = require('fs');
+var mifuHelpers = require('./helpers/mifuHelpersNew.js');
+var instrumentHelpers = require('./helpers/instrumentHelpers.js');
+var vendorDocHelpers = require('./helpers/vendorDocHelpers.js');
+var isometricHelpers = require('./helpers/isometricHelpers.js');
+
+var vdb = xlsx.parse('./sourceFiles/Extrait_VDB.xlsx');
+vdb[0].data.shift();
+var spi = xlsx.parse('./sourceFiles/Extrait_SPI.xlsx');
+spi[0].data.shift();
+var pdms = xlsx.parse('./sourceFiles/Extrait_PDMS.xls');
+pdms[0].data.shift();
+var bom = xlsx.parse('./sourceFiles/BOM.xlsx');
+bom[0].data.shift();
+var impactedIso = xlsx.parse('./sourceFiles/Impacted_iso.xlsx');
+impactedIso[0].data.shift();
+var previousMIFU = xlsx.parse('./sourceFiles/previous_MIFU.xlsx');
+previousMIFU[0].data.shift();
+
+var unicIsoName = 'P00-30-HL-1020-0062-BD20H-HF1-1';
+
+//if (typeof previousMIFU == 'undefined') {
+  var tempData = [];
+//}
+var vendorDocs = vendorDocHelpers.importVendorDocs(vdb[0].data);
+var instruments = instrumentHelpers.importInstruments(spi[0].data, pdms[0].data, previousMIFU[0].data);
+var isometrics = isometricHelpers.importIsometrics(bom[0].data, pdms[0].data, impactedIso[0].data);
+isometrics.forEach(function(isometric) {isometric.updateOnHoldCount(instruments, vendorDocs)});
+isometrics.forEach(function(isometric,ind, arr) {isometric.updateOnHoldImpactedIsoCount(arr)});
+isometrics.forEach(function(isometric) {isometric.updateIFCStatus()});
+
+var res = [
+  {name: 'Instrum',
+    data: [
+      ['Tag','Status VDB','Status PDMS']
+    ]
+  },
+  {name: 'Impacted isos',
+    data: [
+      ['Impacted isometric', 'tag of impacting element', 'global status of impacted isometric']
+    ]
+  }
+];
+
+// permet de remplir res[0]
+var tempData = isometricHelpers.uniqueExportFunction(isometrics,instruments, vendorDocs, unicIsoName);
+
+//console.log(tempData[0].slice(0,10));
+
+
+res[0].data = res[0].data.concat(tempData[0]);
+res[1].data = res[1].data.concat(tempData[1]);
+var buffer = xlsx.build(res);
+
+fs.writeFile('./output/'.concat(unicIsoName).concat('_Unic_Iso_report.xlsx'), buffer, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
